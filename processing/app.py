56759fd3ebeec052be4164f95dcd34d7df972381
Desktop home <kamaljitbhuller@gmail.com>
2023-03-29 17:45:54 -0700

    added assignment 2 files, openapi for each service and added health srevice

--- processing/app.py
+++ processing/app.py
@@ -83,7 +83,7 @@ def populate_stats():
 
     # TODO call the /buy GET endpoint of storage, passing last_updated
     # TODO convert result to a json object, loop through and calculate max_buy_price of all recent records
-    events = requests.get(f'http://34.122.125.217/storage/buy?timestamp={last_updated}')
+    events = requests.get(f'http://35.192.222.219/storage/buy?timestamp={last_updated}')
 
     event = events.json()
     for e in event:
@@ -92,7 +92,7 @@ def populate_stats():
             data["max_buy_price"] = e["item_price"]
 
 
-    events = requests.get(f'http://34.122.125.217/storage/sell?timestamp={last_updated}')
+    events = requests.get(f'http://35.192.222.219/storage/sell?timestamp={last_updated}')
 
     event = events.json()
     for e in event:
